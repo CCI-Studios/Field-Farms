ba7221bcf0d47bf43e9ca6ab0f87b9a52e96b30a
Jonathan Bennett <jbennett@ccistudios.com>
2011-11-27 21:52:41 -0500

    Added gallery to deploy script

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,4 +7,4 @@ role :app, "ccistudios.com"
 # directories
 set :deploy_to, "/home/staging/subdomains/ff"
 set :public, "#{deploy_to}/public_html"
-set :extensions, %w[plg_ie6 public template]
+set :extensions, %w[com_slideshow plg_ie6 public template]
